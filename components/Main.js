189cd701720ee0a9e02e51f2c0b3d660030523bd
Terry McMillan <tmcmillan@lipstickonapig.com>
2020-04-21 19:42:14 -0500

    Navigation Part 1

--- components/Main.js
+++ components/Main.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import Directory from './Directory';
-import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from './CampsiteInfo';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          campsites: CAMPSITES,
-          selectedCampsite: null
-        };
-    }
-
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Directory campsites={this.state.campsites} onPress={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <DirectoryNavigator />
             </View>
+        );
     }
 }
 
